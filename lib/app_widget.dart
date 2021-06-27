5b052fd7cff62386a9345e5c66cd92cb24b63ef4
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-06-26 21:24:01 -0300

    Ended reading barcode

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
+import 'modules/insert_boleto/insert_boleto_page.dart';
 import 'modules/login/login_page.dart';
 import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  // Impede de rotacionar as telas
+  AppWidget()
+  {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,6 +35,7 @@ class AppWidget extends StatelessWidget {
         "/home" : (context) => HomePage(),
         "/login" : (context) => LoginPage(),
         "/barcode_scanner" : (context) => BarcodeScannerPage(),
+        "/insert_boleto" : (context) => InsertBoletoPage(),
       }
     );
   }
---
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+class InsertBoletoPage extends StatelessWidget {
+  const InsertBoletoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        leading: BackButton(
+          color: AppColors.input,
+        )
+      ),
+      body: Column(children: [
+        Text("Insert Boleto Page"),
+      ],)
+    );
+  }
+}
