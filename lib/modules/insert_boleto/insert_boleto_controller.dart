5a661494b071f6e469210535a54b1f1830df7f15
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-06-27 11:38:20 -0300

    inserting boleto

--- lib/modules/insert_boleto/insert_boleto_controller.dart
+++ lib/modules/insert_boleto/insert_boleto_controller.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class InsertBoletoController
 {
   final formKey = GlobalKey<FormState>();
+  BoletoModel model = BoletoModel();
 
   String? validateName(String? value) =>
       value?.isEmpty ?? true ? "O nome não pode ser vazio" : null;
@@ -13,8 +16,40 @@ class InsertBoletoController
   String? validateCodigo(String? value) =>
       value?.isEmpty ?? true ? "O código do boleto não pode ser vazio" : null;
 
-  void cadastrarBoleto()
+  void onChange(
+    {
+      String? name,
+      String? dueDate,
+      double? value,
+      String? barcode
+    }
+  )
+  {
+    model = model.copyWith
+    (
+      name: name,
+      dueDate: dueDate,
+      value: value,
+      barcode: barcode
+    );
+  }
+
+  Future<void> saveBoleto() async
+  {
+      final instance = await SharedPreferences.getInstance();
+      final boletos = instance.getStringList("boletos") ?? <String>[];
+      boletos.add(model.toJson());
+      await instance.setStringList("boletos", boletos);
+      print(boletos);
+      return;
+  }
+
+  Future <void> cadastrarBoleto() async
   {
     final form = formKey.currentState;
+    if(form!.validate()){
+      print(model);
+      return saveBoleto();
+    }
   }
 }
